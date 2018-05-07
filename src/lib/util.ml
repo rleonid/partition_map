7a9bcd2501f3772743608d3d4697e714dfa081e7
Leonid Rozenberg <leonidr@gmail.com>
2018-05-07 16:24:54 -0400

    Use symbolic links for the common modules.

--- src/lib/util.ml
+++ src/lib/util.ml
@@ -1,7 +1,1 @@
-
-let invalid_argf ?(prefix="") fmt =
-  Printf.ksprintf invalid_arg ("%s" ^^ fmt) prefix
-
-let string_of_list ?(show_empty=false) ~sep ~f = function
-  | [] -> if show_empty then "[]" else ""
-  | l  -> StringLabels.concat ~sep (ListLabels.map ~f l)
+../common/util.ml
